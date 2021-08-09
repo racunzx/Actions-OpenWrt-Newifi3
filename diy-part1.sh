80e233e78c11a37e215415f81b6c7586a075d697
abe520 <abe520@msn.cn>
2021-08-09 23:30:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+#echo 'src-git r619ac https://github.com/R619AC-OpenWrt/OpenWrt-Packages' >>feeds.conf.default
+
 # 包含openclash , SSR+ 和 passwall等
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'  >>feeds.conf.default
 # passwall依赖
